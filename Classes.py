8a75384245e55d69b790fb6c1e86a343cc2f0e22
Paweł Brzóska <pawel.m.brzoska@gmail.com>
2023-05-13 00:27:23 +0200

    v1.1 basic optymalization

--- Classes.py
+++ Classes.py
@@ -1,20 +1,22 @@
-
 import pandas as pd
 import os
 import sys
+import math
+import copy
 
 #Setting directory
 os.chdir(os.path.dirname(sys.argv[0]))
+resources = ["People", "Food", "Buildings", "Tools", "Materials", "Other", "Safety", "Fun", "Wood","Stone", "Herbs", "Transport"]
 
 class city:
-    def __init__(self, entities = None, number = None): #, people = None, Buildings = None, Tools = None, Food = None, wood = None
+    def __init__(self, entities = None, number = None): # new materials have to be updated here
         self.entities: dict = {}
         self.entities_dict: dict = {}
         self.People: int = 0
         self.Food: int = 0
         self.Buildings: int = 0
         self.Tools: int = 0
-        self.Tools_materials: int = 0
+        self.Materials: int = 0
         self.Other: int = 0
         self.Safety: int = 0
         self.Fun: int = 0
@@ -24,12 +26,10 @@ class city:
         self.Transport: int = 0
         self.df = None
         self.number:str = number
-        
-    #def __str__(self):
-    #    return f"""
-    #            Game: Elf = {self.elf}, double strike = {self.doublestrike}, Total poison in T2 = {self.t2_damage}, 
-    #            Total mana = {self.total_mana} including {self.g_mana} green mana and {self.r_mana} red mana
-    #            """
+        self.cells: dict = {}
+        self.labels: dict = {}
+        self.Suma: float = 0
+        self.problematic: str =""
 
     def Get_entities_dict(self):
         df = pd.read_excel("test.xlsx")
@@ -45,12 +45,12 @@ class city:
         
         self.entities_dict = entities
 
-    def Get_resources(self):
+    def Reset(self):
         self.People = 0
         self.Food = 0
         self.Buildings = 0
         self.Tools = 0
-        self.Tools_materials = 0
+        self.Materials = 0
         self.Other = 0
         self.Safety = 0
         self.Fun = 0
@@ -58,13 +58,18 @@ class city:
         self.Stone = 0
         self.Herbs = 0
         self.Transport = 0
+        self.Suma = 0
+
+    def Get_resources(self):
+
+        self.Reset()
 
         for entity in self.entities:
             self.People += self.entities_dict[entity]['People']*self.entities[entity]
             self.Food += self.entities_dict[entity]['Food']*self.entities[entity]
             self.Buildings += self.entities_dict[entity]['Buildings']*self.entities[entity]
             self.Tools += self.entities_dict[entity]['Tools']*self.entities[entity]
-            self.Tools_materials += self.entities_dict[entity]['Tools_materials']*self.entities[entity]
+            self.Materials += self.entities_dict[entity]['Materials']*self.entities[entity]
             self.Other += self.entities_dict[entity]['Other']*self.entities[entity]
             self.Safety += self.entities_dict[entity]['Safety']*self.entities[entity]
             self.Fun += self.entities_dict[entity]['Fun']*self.entities[entity]
@@ -73,14 +78,65 @@ class city:
             self.Herbs += self.entities_dict[entity]['Herbs']*self.entities[entity]
             self.Transport += self.entities_dict[entity]['Transport']*self.entities[entity]
 
+    def Update_resources_up(self):
+        ranking = {}
+        
+        City2 = city()
+        
+        for entity in self.entities:
+            Suma = 0
+            City2 = copy.copy(self)
+            City2.Get_resources()
+            City2.People += self.entities_dict[entity]['People']
+            City2.Food += self.entities_dict[entity]['Food']
+            City2.Buildings += self.entities_dict[entity]['Buildings']
+            City2.Tools += self.entities_dict[entity]['Tools']
+            City2.Materials += self.entities_dict[entity]['Materials']
+            City2.Other += self.entities_dict[entity]['Other']
+            City2.Safety += self.entities_dict[entity]['Safety']
+            City2.Fun += self.entities_dict[entity]['Fun']
+            City2.Wood += self.entities_dict[entity]['Wood']
+            City2.Stone += self.entities_dict[entity]['Stone']
+            City2.Herbs += self.entities_dict[entity]['Herbs']
+            City2.Transport += self.entities_dict[entity]['Transport']
+            for i, res in enumerate(resources):
+                City2.Suma += getattr(City2, res)**2
+            City2.Suma -= getattr(City2, "People")**2
+            Suma = City2.Suma/City2.People # This is basically a optimization function. As city size inflated margin of error, we get an error per person
+            Suma = round(math.log10(Suma),3) # And as it is HUGE often we get log10 to comprehend this number ;D
+            
+            ranking[entity] = Suma
+            sorted_tests = sorted(ranking.items(), key=lambda x: x[1])
 
+            #if tested_suma < New_suma: 
+            #    New_suma = tested_suma
+            #    New_entity = entity
+            
+        return(sorted_tests[0][0])
 
+    def Update_resources_down(self):
+        New_suma = self.Suma
+        New_entity = ""
+        for entity in self.entities:
+            tested_suma = New_suma
+            tested_suma -= self.entities_dict[entity]['Food']**2
+            tested_suma -= self.entities_dict[entity]['Buildings']**2
+            tested_suma -= self.entities_dict[entity]['Tools']**2
+            tested_suma -= self.entities_dict[entity]['Materials']**2
+            tested_suma -= self.entities_dict[entity]['Other']**2
+            tested_suma -= self.entities_dict[entity]['Safety']**2
+            tested_suma -= self.entities_dict[entity]['Fun']**2
+            tested_suma -= self.entities_dict[entity]['Wood']**2
+            tested_suma -= self.entities_dict[entity]['Stone']**2
+            tested_suma -= self.entities_dict[entity]['Herbs']**2
+            tested_suma -= self.entities_dict[entity]['Transport']**2
+            tested_suma = tested_suma/(self.People+self.entities_dict[entity]['People'])
+            tested_suma = round(math.log10(tested_suma),3)
+            if tested_suma < New_suma: 
+                New_suma = tested_suma
+                New_entity = entity
 
-    def Run(self):
-        #TU jakoś zrobić żeby sprawdzał czego brakuje i dorzucał rzeczy.
-        print(city())
-
-
+        return(New_entity)
 
 if __name__ == "__main__":
     pass
--- Main.py
+++ Main.py
@@ -7,12 +7,14 @@ import sys
 import Classes
 import string
 import ctypes
+import math
+import copy
 
 # Increase Dots Per inch so it looks sharper
 ctypes.windll.shcore.SetProcessDpiAwareness(True)
 
 workshops = ["Farmy", "Łowcy", "Gotowanie", "Architekci", "Kowale", "Metalurgia", "Cieśla", "Alchemik", "Tkacz", "Kamieniarz", "Drwal", "Zbieracz"]
-resources = ["People", "Food", "Buildings", "Tools", "Tools_materials", "Other", "Safety", "Fun", "Wood","Stone", "Herbs", "Transport"]
+resources = ["People", "Food", "Buildings", "Tools", "Materials", "Other", "Safety", "Fun", "Wood","Stone", "Herbs", "Transport"]
 
 #Setting directory
 os.chdir(os.path.dirname(sys.argv[0]))
@@ -28,21 +30,22 @@ def Start(City):
     frm = ttk.Frame(window, padding=10) #Creating the GUI frame
     frm.grid()
 
-    Label(frm, text="CO\lvl").grid(column=0, row=0)
+    #Label(frm, text="CO\lvl").grid(column=0, row=0)
 
     # Define X and Y Axis Lists
     xAxis = range(1, 6)
     yAxis = range(0, 12)
-    cells = {}
-    labels = {}
+    City.cells = {}
+    City.labels = {}
+
     ###########################################
     #            Workshops labels             #
     ###########################################
      
     for i, y in enumerate(workshops):
-        label = Label(frm, text = y, width=10, anchor="w").grid(row=i + 1, column=0)
+        label = Label(frm, text = y, width=12, anchor="w").grid(row=i + 1, column=0)
     for i, x in enumerate(xAxis):
-        label = Label(frm, text = x, width=5, anchor="n").grid(row=0, column=i + 1)
+        label = Label(frm, text = x, width=6, anchor="n").grid(row=0, column=i + 1)
     
     ###########################################
     #             Workshops Cells             #
@@ -54,10 +57,10 @@ def Start(City):
             id = f'{workshop}_{x}'
             var = StringVar(frm, City.df[id], id)
             # Make Entry and label, offset each axis by one because of the lables
-            e = Entry(frm, textvariable=var, width=5)
+            e = Entry(frm, textvariable=var, width=6)
             e.grid(row=i + 1, column=xcoor + 1)
             
-            cells[id] = var
+            City.cells[id] = var
     
     ###########################################
     #              Resources sums             #
@@ -66,68 +69,79 @@ def Start(City):
     for i, res in enumerate(resources):
         id = f'{res}'
         var = StringVar(frm, "", id)
-        Label(frm, text=f"{res} = ", width=15, anchor="w").grid(column=0, row=13+i,columnspan=2)
-        Label(frm, textvariable=var, width=5, anchor="e").grid(column=2, row=13+i)
-        labels[id] = var
+        Label(frm, text=f"{res} = ", width=12, anchor="w").grid(column=0, row=13+i)
+        Label(frm, textvariable=var, width=6, anchor="e").grid(column=1, row=13+i)
+        City.labels[id] = var
 
     var = StringVar(frm, "", "suma")
-    Label(frm, text = "Suma = ", width=10, anchor="w").grid(column=0, row=25)
-    Label(frm, textvariable=var, width=10, anchor="e").grid(column=1, row=25,columnspan=2)
-    labels["suma"] = var
+    Label(frm, text = "Suma = ", width=12, anchor="w").grid(column=0, row=25)
+    Label(frm, textvariable=var, width=6, anchor="e").grid(column=1, row=25)
+    City.labels["suma"] = var
 
     ###########################################
     #                 Exports                 #
     ###########################################
 
-    Label(frm, text="Export", width=10, anchor="n").grid(column=3, row=13,columnspan=3)
+    Label(frm, text="Export", width=12, anchor="n").grid(column=2, row=13,columnspan=4)
     for i, (name, value) in enumerate(City.df.filter(like = 'Export').items()):
-        Label(frm, text=name[7:], width=10, anchor="w").grid(column=3, row=i+14,columnspan=2)
+        Label(frm, text=name[7:], width=12, anchor="w").grid(column=2, row=i+14,columnspan=2)
         id = name
         var = StringVar(frm, value, id)
-        e = Entry(frm, textvariable=var, width=5)
-        e.grid(column=5, row=i+14)
-        cells[id] = var
+        e = Entry(frm, textvariable=var, width=12)
+        e.grid(column=4, row=i+14, columnspan=2)
+        City.cells[id] = var
 
     ###########################################
     #             Additional cells            #
     ###########################################
 
     for i, (name, value) in enumerate(City.df['kobiety pracujące (inne)':'uprawa ziela 4 poz'].items()):
-        Label(frm, text=name, width=40, anchor="w").grid(column=8, row=i)
+        Label(frm, text=name, width=30, anchor="w").grid(column=8, row=i)
         id = name
         var = StringVar(frm, value, id)
         e = Entry(frm, textvariable=var, width=5)
         e.grid(column=9, row=i)
-        cells[id] = var
+        City.cells[id] = var
+
+    ###########################################
+    #                Functions                #
+    ###########################################
 
     def Load_from_file():
-        for cell in cells:
-            cells[cell].set(City.df[cell])
-        
+        for cell in City.cells:
+            City.cells[cell].set(City.df[cell])
+    
     def Push_to_city():
-        print(City.entities_dict)
+        City.Suma = 0
         Suma = 0
-        for cell in cells:
-            City.entities[cell] = int(cells[cell].get())
+        for cell in City.cells:
+            City.entities[cell] = int(City.cells[cell].get())
 
         City.Get_resources()
         for i, res in enumerate(resources):
             text = getattr(City, res)
-            labels[res].set(text)
-            Suma += getattr(City, res)**2
-        labels["suma"].set(Suma)
+            City.labels[res].set(text)
+            City.Suma += getattr(City, res)**2
+        City.Suma -= getattr(City, "People")**2
+        Suma = City.Suma/City.People # This is basically a optimization function. As city size inflated margin of error, we get an error per person
+        Suma = round(math.log10(Suma),3) # And as it is HUGE often we get log10 to comprehend this number ;D
+        City.labels["suma"].set(Suma)
+
+    def Optimize_up():
+        value = int(City.cells[City.Update_resources_up()].get())
+        value+=1
+        City.cells[City.Update_resources_up()].set(value)
+        Push_to_city()
 
     Button(frm, text="Load", command=Load_from_file).grid(column=6, row=0)
     Button(frm, text="Push", command=Push_to_city).grid(column=6, row=1)
-
-
-    
+    Button(frm, text="Opt_up", command=Optimize_up).grid(column=6, row=2)
 
     window.mainloop()
 
 if __name__ == "__main__":
 
-    City = Classes.city(number = 4)
+    City = Classes.city(number = 0)
     City.df = Get_city(number = City.number)
     City.Get_entities_dict()
 
